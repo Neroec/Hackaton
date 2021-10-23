34eced74018b29938c1019b1234be3c84be576d0
Neroec <61140152+Neroec@users.noreply.github.com>
2021-10-23 12:02:15 +0300

    Update main.php

--- Code/main.py
+++ Code/main.py
@@ -1,11 +1,7 @@
 import osmnx as ox
 
 
-def generate_edges():
-    G = ox.graph_from_place('Lipetsk, Russia', 'drive',
-                            custom_filter='["highway"~"residential|tertiary|secondary|primary"]',
-                            )
-    _, edges = ox.graph_to_gdfs(G)
+def generate_edges(edges):
     f = open("edges.txt", 'w')
     for edge in edges.values:
         coords = edge[5].coords # 5 for custom | 6 for drive
@@ -16,13 +12,14 @@ def generate_edges():
         f.write('{l}'.format(l=line))
 
 
-def generate_nodes():
-    G = ox.graph_from_place('Lipetsk, Russia', 'drive',
-                            custom_filter='["highway"~"residential|tertiary|secondary|primary"]')
-    nodes, _ = ox.graph_to_gdfs(G)
+def generate_nodes(nodes):
     f = open("nodes.txt", 'w')
     for node in nodes.values:
         f.write('[{x}, {y}],\n'.format(x=node[0], y=node[1]))
 
 
-generate_edges()
+G = ox.graph_from_place('Lipetsk, Russia', 'drive',
+                            custom_filter='["highway"~"residential|tertiary|secondary|primary"]')
+nodes, edges = ox.graph_to_gdfs(G)
+generate_edges(edges)
+generate_nodes(nodes)
